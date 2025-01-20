c5b05f68f7cdebb329f376ecd6d86d7417bb6281
Rick Macklem <rmacklem@FreeBSD.org>
2025-01-20 13:51:33 -0800

    nfsd: Add support for the NFSv4.2 change_attr_type attribute
    Richard Kojedzinszky reported an intermittent problem where
    the Linux NFSv4.2 client would sometimes not see changes done
    to a directory by another client, although the change attribute
    for the directory had changed.
    
    A test patch that added the change_attr_type attribute to the
    server and always returned NFS4_CHANGE_TYPE_VERSION_COUNTER_NOPNFS
    seems to have resolved the issue.  Somewhat oddly, the Linux
    knfsd server does not support this attribute but does not
    seem to exhibit the stale caching problem.
    
    This patch uses the VFCF_FILEREVINC flag on a file system (UFS, ZFS)
    to return NFS4_CHANGE_TYPE_VERSION_COUNTER_NOPNFS.  It also
    returns NFS4_CHANGE_TYPE_TIME_METADATA if VFCF_FILEREVCT is set,
    which may be useful for exported fuse file systems.
    
    PR:	284186
    Reported by:	Richard Kojedzinszky <richard@kojedz.in>
    Tested by:	Richard Kojedzinszky <richard@kojedz.in>
    MFC after:	2 weeks

--- sys/fs/nfs/nfs_commonsubs.c
+++ sys/fs/nfs/nfs_commonsubs.c
@@ -2302,6 +2302,23 @@ nfsv4_loadattr(struct nfsrv_descript *nd, vnode_t vp,
 			if (compare && !(*retcmpp) && i != nfs_srvmaxio)
 				*retcmpp = NFSERR_NOTSAME;
 			break;
+		case NFSATTRBIT_CHANGEATTRTYPE:
+			NFSM_DISSECT(tl, uint32_t *, NFSX_UNSIGNED);
+			if (compare) {
+				if (!(*retcmpp)) {
+				    tuint = NFSV4CHANGETYPE_UNDEFINED;
+				    if ((vp->v_mount->mnt_vfc->vfc_flags &
+					VFCF_FILEREVINC) != 0)
+					tuint = NFSV4CHANGETYPE_VERS_COUNTER_NOPNFS;
+				    else if ((vp->v_mount->mnt_vfc->vfc_flags &
+					VFCF_FILEREVCT) != 0)
+					tuint = NFSV4CHANGETYPE_TIME_METADATA;
+				    if (fxdr_unsigned(uint32_t, *tl) != tuint)
+					*retcmpp = NFSERR_NOTSAME;
+				}
+			}
+			attrsum += NFSX_UNSIGNED;
+			break;
 		default:
 			printf("EEK! nfsv4_loadattr unknown attr=%d\n",
 				bitpos);
@@ -3112,6 +3129,21 @@ nfsv4_fillattr(struct nfsrv_descript *nd, struct mount *mp, vnode_t vp,
 				*tl = newnfs_true;
 			else
 				*tl = newnfs_false;
+			retnum += NFSX_UNSIGNED;
+			break;
+		case NFSATTRBIT_CHANGEATTRTYPE:
+			NFSM_BUILD(tl, uint32_t *, NFSX_UNSIGNED);
+			*tl = txdr_unsigned(NFSV4CHANGETYPE_UNDEFINED);
+			if (mp != NULL) {
+				if ((mp->mnt_vfc->vfc_flags &
+				    VFCF_FILEREVINC) != 0)
+					*tl = txdr_unsigned(
+					   NFSV4CHANGETYPE_VERS_COUNTER_NOPNFS);
+				else if ((mp->mnt_vfc->vfc_flags &
+				    VFCF_FILEREVCT) != 0)
+					*tl = txdr_unsigned(
+					   NFSV4CHANGETYPE_TIME_METADATA);
+			}
 			retnum += NFSX_UNSIGNED;
 			break;
 		default:
