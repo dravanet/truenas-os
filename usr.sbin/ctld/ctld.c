rkojedzinszky/os

ctld: change configuration live

usr.sbin/ctld/ctld.c
@@ -54,6 +54,7 @@ __FBSDID("$FreeBSD$");
 
 #include "ctld.h"
 #include "isns.h"
+#include "control.h"
 
 bool proxy_mode = false;
 
@@ -68,7 +69,7 @@ static void
 usage(void)
 {
 
-	fprintf(stderr, "usage: ctld [-d][-u][-f config-file]\n");
+	fprintf(stderr, "usage: ctld [-d][-u][-f config-file][-c control-socket]\n");
 	exit(1);
 }
 
@@ -150,7 +151,7 @@ auth_new(struct auth_group *ag)
 	return (auth);
 }
 
-static void
+void
 auth_delete(struct auth *auth)
 {
 	TAILQ_REMOVE(&auth->a_auth_group->ag_auths, auth, a_next);
@@ -2101,12 +2102,6 @@ conf_apply(struct conf *oldconf, struct conf *newconf)
 	TAILQ_FOREACH(newpg, &newconf->conf_portal_groups, pg_next) {
 		if (newpg->pg_foreign)
 			continue;
-		if (newpg->pg_unassigned) {
-			log_debugx("not listening on portal-group \"%s\", "
-			    "not assigned to any target",
-			    newpg->pg_name);
-			continue;
-		}
 		TAILQ_FOREACH(newp, &newpg->pg_portals, p_next) {
 			/*
 			 * Try to find already open portal and reuse
@@ -2497,6 +2492,7 @@ found:
 				TAILQ_FOREACH(portal, &pg->pg_portals, p_next)
 					nfds = fd_add(portal->p_socket, &fdset, nfds);
 			}
+			nfds = control_add_fds(&fdset, nfds);
 			error = select(nfds + 1, &fdset, NULL, NULL, NULL);
 			if (error <= 0) {
 				if (errno == EINTR)
@@ -2524,6 +2520,7 @@ found:
 					break;
 				}
 			}
+			control_handle_fds(conf, &fdset);
 #ifdef ICL_KERNEL_PROXY
 		}
 #endif
@@ -2691,8 +2688,9 @@ main(int argc, char **argv)
 	int debug = 0, ch, error;
 	bool dont_daemonize = false;
 	bool use_ucl = false;
+	const char *control_socket = NULL;
 
-	while ((ch = getopt(argc, argv, "duf:R")) != -1) {
+	while ((ch = getopt(argc, argv, "duf:Rc:")) != -1) {
 		switch (ch) {
 		case 'd':
 			dont_daemonize = true;
@@ -2710,6 +2708,9 @@ main(int argc, char **argv)
 			    "does not support iSER protocol");
 #endif
 			proxy_mode = true;
+			break;
+		case 'c':
+			control_socket = optarg;
 			break;
 		case '?':
 		default:
@@ -2741,6 +2742,13 @@ main(int argc, char **argv)
 	oldconf = NULL;
 
 	register_signals();
+
+	// Open control-socket
+	if (control_socket != NULL) {
+		if (control_init(control_socket) != 0) {
+			log_errx(2, "failed to open control-socket; exiting");
+		}
+	}
 
 	if (dont_daemonize == false) {
 		log_debugx("daemonizing");
@@ -2784,6 +2792,8 @@ main(int argc, char **argv)
 
 			log_debugx("removing CTL iSCSI ports "
 			    "and terminating all connections");
+
+			control_shutdown();
 
 			oldconf = newconf;
 			newconf = conf_new();

dravanet/truenas-os

usr.sbin/ctld/ctld.c
@@ -51,6 +51,7 @@
 
 #include "ctld.h"
 #include "isns.h"
+#include "control.h"
 
 static bool	timed_out(void);
 #ifdef ICL_KERNEL_PROXY
@@ -81,8 +82,8 @@ static void
 usage(void)
 {
 
-	fprintf(stderr, "usage: ctld [-d][-u][-f config-file]\n");
-	fprintf(stderr, "       ctld -t [-u][-f config-file]\n");
+	fprintf(stderr, "usage: ctld [-d][-u][-f config-file][-c control-socket]\n");
+	fprintf(stderr, "       ctld -t [-u][-f config-file][-c control-socket]\n");
 	exit(1);
 }
 
@@ -149,7 +150,7 @@ auth_new(struct auth_group *ag)
 	return (auth);
 }
 
-static void
+void
 auth_delete(struct auth *auth)
 {
 	TAILQ_REMOVE(&auth->a_auth_group->ag_auths, auth, a_next);
@@ -2135,12 +2136,6 @@ conf_apply(struct conf *oldconf, struct conf *newconf)
 	TAILQ_FOREACH(newpg, &newconf->conf_portal_groups, pg_next) {
 		if (newpg->pg_foreign)
 			continue;
-		if (newpg->pg_unassigned) {
-			log_debugx("not listening on portal-group \"%s\", "
-			    "not assigned to any target",
-			    newpg->pg_name);
-			continue;
-		}
 		TAILQ_FOREACH(newp, &newpg->pg_portals, p_next) {
 			/*
 			 * Try to find already open portal and reuse
@@ -2561,6 +2556,7 @@ found:
 				TAILQ_FOREACH(portal, &pg->pg_portals, p_next)
 					nfds = fd_add(portal->p_socket, &fdset, nfds);
 			}
+			nfds = control_add_fds(&fdset, nfds);
 			error = select(nfds + 1, &fdset, NULL, NULL, NULL);
 			if (error <= 0) {
 				if (errno == EINTR)
@@ -2588,6 +2584,7 @@ found:
 					break;
 				}
 			}
+			control_handle_fds(conf, &fdset);
 #ifdef ICL_KERNEL_PROXY
 		}
 #endif
@@ -2804,8 +2801,9 @@ main(int argc, char **argv)
 	bool dont_daemonize = false;
 	bool test_config = false;
 	bool use_ucl = false;
+	const char *control_socket = "/var/run/ctld.sock";
 
-	while ((ch = getopt(argc, argv, "dtuf:R")) != -1) {
+	while ((ch = getopt(argc, argv, "dtuf:Rc:")) != -1) {
 		switch (ch) {
 		case 'd':
 			dont_daemonize = true;
@@ -2826,6 +2824,13 @@ main(int argc, char **argv)
 			    "does not support iSER protocol");
 #endif
 			proxy_mode = true;
+			break;
+		case 'c':
+			if (strcmp(optarg, "") == 0) {
+				control_socket = NULL;
+			} else {
+				control_socket = optarg;
+			}
 			break;
 		case '?':
 		default:
@@ -2879,6 +2884,13 @@ main(int argc, char **argv)
 	conf_delete(oldconf);
 	oldconf = NULL;
 
+	// Open control-socket
+	if (control_socket != NULL) {
+		if (control_init(control_socket) != 0) {
+			log_errx(2, "failed to open control-socket; exiting");
+		}
+	}
+
 	if (dont_daemonize == false) {
 		log_debugx("daemonizing");
 		if (daemon(0, 0) == -1) {
@@ -2925,6 +2937,8 @@ main(int argc, char **argv)
 
 			log_debugx("removing CTL iSCSI ports "
 			    "and terminating all connections");
+
+			control_shutdown();
 
 			oldconf = newconf;
 			newconf = conf_new();